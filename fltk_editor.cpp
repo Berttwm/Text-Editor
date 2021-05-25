10d52a08a249545aefe7d6607a94e38dbe252b44
berttwm <bertrandtwm@gmail.com>
2021-05-26 02:05:10 +0800

    added new,save,saveas,open,insert,view and close functions

--- fltk_editor.cpp
+++ fltk_editor.cpp
@@ -13,6 +13,7 @@
 #include <FL/Fl_Input.H>
 #include <FL/FL_Menu_Bar.H>
 #include <FL/Fl_Menu_Item.H>
+#include <FL/Fl_Native_File_Chooser.H>
 #include <FL/Fl_Return_Button.H>
 #include <FL/Fl_Window.h>
 #include <FL/Fl_Text_Buffer.H>
@@ -83,6 +84,8 @@ void find2_cb(Fl_Widget*, void*);
 void replace_cb(Fl_Widget*, void*);
 void replace2_cb(Fl_Widget*, void*);
 void changed_cb(int, int nInserted, int nDeleted, int, const char*, void* v);
+int check_save(void);
+void save_file(char *newfile);
 void load_file(char *newfile, int ipos);
 void set_title(Fl_Window* w);
 void style_update(int pos, int nInserted, int nDeleted, int, const char *, void *cbArg);
@@ -137,7 +140,99 @@ int main(int argc, char **argv)
 
 }
 
-void load_file(char *newfile, int ipos) {
+/* Menu Bar Functions */
+void new_cb(Fl_Widget*, void*) {
+	if (!check_save()) return;
+
+	filename[0] = '\0';
+	textbuf->select(0, textbuf->length());
+	textbuf->remove_selection();
+	changed = 0;
+	textbuf->call_modify_callbacks();
+}
+void save_cb(void) {
+	if (filename[0] == '\0') {
+		// No filename - get one!
+		saveas_cb();
+		return;
+	}
+	else save_file(filename);
+}
+void saveas_cb() {
+	Fl_Native_File_Chooser fnfc;
+	fnfc.title("What would you like to name your save file?");
+	fnfc.type(Fl_Native_File_Chooser::BROWSE_SAVE_FILE);
+	if (fnfc.show()) return;
+	save_file(fnfc.filename());
+}
+void open_cb(Fl_Widget*, void*) {
+	if (!check_save()) return;
+	Fl_Native_File_Chooser fnfc;
+	fnfc.title("Open file");
+	fnfc.type(Fl_Native_File_Chooser::BROWSE_FILE);
+	if (fnfc.show()) return;
+	load_file(fnfc.filename(), -1);
+}
+void insert_cb(Fl_Widget*, void* v) {
+	Fl_Native_File_Chooser fnfc;
+	fnfc.title("Insert File");
+	fnfc.type(Fl_Native_File_Chooser::BROWSE_FILE);
+	if (fnfc.show()) return;
+	EditorWindow *w = (EditorWindow *)v;
+	load_file(fnfc.filename(), w->editor->insert_position());
+
+}
+void view_cb(Fl_Widget*, void*) {
+	Fl_Window* w = new_view();
+	w->show();
+}
+void close_cb(Fl_Widget*, void* v) {
+	EditorWindow* w = (EditorWindow*)v;
+
+	if (num_windows == 1) {
+		if (!check_save())
+			return;
+	}
+
+	w->hide();
+	w->editor->buffer(0);
+	textbuf->remove_modify_callback(style_update, w->editor);
+	textbuf->remove_modify_callback(changed_cb, w);
+	Fl::delete_widget(w);
+
+	num_windows--;
+	if (!num_windows) exit(0);
+}
+void quit_cb(Fl_Widget*, void*) {
+	if (changed && !check_save())
+		return;
+
+	exit(0);
+}
+/* File Manipulation Functions */
+int check_save(void) {
+	if (!changed) return 1;
+
+	int r = fl_choice("The current file has not been saved.\n"
+		"Would you like to save it now?",
+		"Cancel", "Save", "Discard");
+
+	if (r == 1) {
+		save_cb(); // Save the file...
+		return !changed;
+	}
+
+	return (r == 2) ? 1 : 0;
+}
+void save_file(const char *newfile) {
+	if (textbuf->savefile(newfile))
+		fl_alert("Error writing to file \'%s\':\n%s.", newfile, strerror(errno));
+	else
+		strcpy(filename, newfile);
+	changed = 0;
+	textbuf->call_modify_callbacks();
+}
+void load_file(const char *newfile, int ipos) {
 	loading = 1;
 	int insert = (ipos != -1);
 	changed = insert;
@@ -206,6 +301,7 @@ void set_title(Fl_Window* w) {
 	w->label(title);
 }
 
+/* Special Styling Functions */
 void style_update(int pos,		// I - Position of update
 	int nInserted,	// I - Number of inserted chars
 	int nDeleted,	// I - Number of deleted chars
