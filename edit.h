782d1b7d13ff940d4135fce0dbc0eded782a4eed
berttwm <bertrandtwm@gmail.com>
2021-05-28 00:59:14 +0800

    build 2

--- edit.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <FL/Fl.h>
-
-void undo_cb(Fl_Widget*, void*);
-void cut_cb(Fl_Widget*, void*);
-void copy_cb(Fl_Widget*, void*);
-void paste_cb(Fl_Widget*, void*);
-void delete_cb(Fl_Widget*, void*);
--- search.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <FL/Fl.h>
-
-void find_cb(Fl_Widget*, void*);
-void find2_cb(Fl_Widget*, void*);
-void replace_cb(Fl_Widget*, void*);
-void replace2_cb(Fl_Widget*, void*);
-void replall_cb(Fl_Widget*, void* v);
-void replcan_cb(Fl_Widget*, void* v);
